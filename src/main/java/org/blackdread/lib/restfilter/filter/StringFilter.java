5b72128379b0f643d53e66577685508fd9fb0bda
Yoann CAPLAIN <yoann.caplain@free.fr>
2019-10-02 21:59:58 +0800

    Add notContains

--- src/main/java/org/blackdread/lib/restfilter/filter/StringFilter.java
+++ src/main/java/org/blackdread/lib/restfilter/filter/StringFilter.java
@@ -8,10 +8,13 @@ import java.util.Objects;
  * It can be added to a criteria class as a member, to support the following query parameters:
  * <code>
  * fieldName.equals='something'
+ * fieldName.notEquals='something'
  * fieldName.specified=true
  * fieldName.specified=false
  * fieldName.in='something','other'
+ * fieldName.notIn='something','other'
  * fieldName.contains='thing'
+ * fieldName.notContains='thing'
  * </code>
  */
 public class StringFilter extends Filter<String> {
@@ -19,6 +22,7 @@ public class StringFilter extends Filter<String> {
     private static final long serialVersionUID = 1L;
 
     private String contains;
+    private String notContains;
 
     public StringFilter() {
     }
@@ -26,6 +30,7 @@ public class StringFilter extends Filter<String> {
     public StringFilter(final StringFilter filter) {
         super(filter);
         this.contains = filter.contains;
+        this.notContains = filter.notContains;
     }
 
     /**
@@ -81,6 +86,15 @@ public class StringFilter extends Filter<String> {
         return this;
     }
 
+    public String getNotContains() {
+        return notContains;
+    }
+
+    public StringFilter setNotContains(String notContains) {
+        this.notContains = notContains;
+        return this;
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -93,19 +107,22 @@ public class StringFilter extends Filter<String> {
             return false;
         }
         final StringFilter that = (StringFilter) o;
-        return Objects.equals(contains, that.contains);
+        return Objects.equals(contains, that.contains) &&
+            Objects.equals(notContains, that.notContains);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), contains);
+        return Objects.hash(super.hashCode(), contains, notContains);
     }
 
     @Override
     public String toString() {
         return getFilterName() + " ["
             + (getContains() != null ? "contains=" + getContains() + ", " : "")
+            + (getNotContains() != null ? "notContains=" + getNotContains() + ", " : "")
             + (getEquals() != null ? "equals=" + getEquals() + ", " : "")
+            + (getNotEquals() != null ? "notEquals=" + getNotEquals() + ", " : "")
             + (getSpecified() != null ? "specified=" + getSpecified() : "")
             + "]";
     }
