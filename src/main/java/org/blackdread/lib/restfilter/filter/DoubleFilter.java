91917f5dbf74613d01cf1114f788cdc368b0ddfe
yoann CAPLAIN <jojo.julien@gmail.com>
2019-11-13 10:13:51 +0800

    Inform licence copy

--- src/main/java/org/blackdread/lib/restfilter/filter/DoubleFilter.java
+++ src/main/java/org/blackdread/lib/restfilter/filter/DoubleFilter.java
@@ -20,6 +20,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * Original taken from jhipster project, this class has been modified.
  */
 package org.blackdread.lib.restfilter.filter;
 
